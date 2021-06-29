ed36f58748773a537c2afbc8b7e77f49531f3fc2
pgreen <philip.green@q2ebanking.com>
2021-06-29 15:55:22 -0500

    view binding

--- app/src/main/java/pgreen/huescales/LauncherActivity.kt
+++ app/src/main/java/pgreen/huescales/LauncherActivity.kt
@@ -1,11 +1,19 @@
 package pgreen.huescales
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pgreen.huescales.huepitch.PitchDetectorActivity
 
-class MainActivity : AppCompatActivity() {
+class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_launcher)
+
+        startInitialActivity()
+    }
+
+    fun startInitialActivity() {
+        startActivity(Intent(this, PitchDetectorActivity::class.java))
     }
 }
--- app/src/main/java/pgreen/huescales/huepitch/PitchDetectorActivity.kt
+++ app/src/main/java/pgreen/huescales/huepitch/PitchDetectorActivity.kt
@@ -2,10 +2,17 @@ package pgreen.huescales.huepitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pgreen.huescales.R
+import pgreen.huescales.databinding.ActivityPitchDetectorBinding
 
 class PitchDetectorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPitchDetectorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pitch_detector)
+        binding = ActivityPitchDetectorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.pitchDetectionButton.text = getString(R.string.start)
     }
 }
