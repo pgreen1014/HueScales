3a9caad44c68d15b855be9efb24b9bc823e9ae38
pgreen <philip.green@q2ebanking.com>
2021-06-29 15:53:59 -0500

    Creating initial pitch detector activity

---
+++ app/src/main/java/pgreen/huescales/huepitch/PitchDetectorActivity.kt
@@ -0,0 +1,11 @@
+package pgreen.huescales.huepitch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PitchDetectorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pitch_detector)
+    }
+}
